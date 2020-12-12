2d6e9633f9ac871e77a0f8d25a3933edcdf9da19
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-13 00:04:49 +0200

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import { refs } from './js/refs';
-import { fetchImages } from './js/apiService';
+import imageService from './js/apiService';
 import { updateMarkup } from './js/updateMarkup';
 import 'material-design-icons/iconfont/material-icons.css';
 import './styles.css';
 
-let searchQuery = '';
-let page = 1;
+refs.loadMoreBtn.classList.add('is-hidden');
 
 refs.searchForm.addEventListener('submit', event => {
   event.preventDefault();
   const form = event.currentTarget;
-  searchQuery = form.elements.query.value;
+  imageService.query = form.elements.query.value;
   refs.galleryList.innerHTML = '';
   form.reset(); //clear input value
-  page = 1;
-  fetchImages(searchQuery, page).then(hits => {
+  imageService.resetPage();
+ 
+
+  imageService.fetchImages().then(hits => {
     updateMarkup(hits);
-    page += 1;
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    
   });
 });
 
 refs.loadMoreBtn.addEventListener('click', () => {
-  fetchImages(searchQuery, page).then(hits => {
+  imageService.fetchImages().then(hits => {
     updateMarkup(hits);
-    page += 1;
   });
 });
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,17 +1,33 @@
 const apiKey = '19499412-b0a970c3da683bab1682cc3ad';
 const baseUrl = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
 
-// export default {
-// searchQuery: '',
-// page: 1,
+export default {
+  searchQuery: '',
+  page: 1,
 
-  
-export function fetchImages(searchQuery, page=1) {
-const url = `${baseUrl}&q=${searchQuery}&page=${page}&per_page=12&key=${apiKey}`;
-return fetch(url)
-  .then(response => response.json())
-  .then(({hits}) => hits);
-}
+  fetchImages() {
+    const url = `${baseUrl}&q=${this.query}&page=${this.page}&per_page=12&key=${apiKey}`; // this.query => getter
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage;
+        return hits;
+      });
+  },
 
+  incrementPage() {
+    this.page += 1;
+  },
 
+  resetPage() {
+    this.page = 1;
+  },
 
+  get query() {
+    return this.searchQuery;
+  },
+
+  set query(value) {
+    return (this.searchQuery = value);
+  },
+};
