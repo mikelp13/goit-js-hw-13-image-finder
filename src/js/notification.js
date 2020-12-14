06ae7a3560aec1504b7f11a1f741290b90b785f0
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2020-12-14 14:44:20 +0200

    added refactored notification code and styles

--- src/js/notification.js
+++ src/js/notification.js
@@ -2,50 +2,37 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { info, error, Stack, notice } from '@pnotify/core';
 
+const options = {
+  mode: 'light',
+  styling: 'brighttheme',
+  delay: 2000,
+  maxTextHeight: null,
+  stack: new Stack({
+    dir1: 'down',
+    dir2: 'right', // Position from the top left corner.
+    firstpos1: 10,
+    firstpos2: 10, // 10px from the top, 10px from the left.
+  })
+}
+
 function queryInfo() {
   return info({
     text: 'Please enter a search query.',
-    mode: 'light',
-    styling: 'brighttheme',
-    delay: 2000,
-    maxTextHeight: null,
-    stack: new Stack({
-      dir1: 'down',
-      dir2: 'right', // Position from the top left corner.
-      firstpos1: 10,
-      firstpos2: 10, // 10px from the top, 10px from the left.
-    }),
-  });
+    ...options,
+    });
 }
+
 function queryNotice() {
   return notice({
     text: 'Sorry, nothing was found :(',
-    mode: 'light',
-    styling: 'brighttheme',
-    delay: 2000,
-    maxTextHeight: null,
-    stack: new Stack({
-      dir1: 'down',
-      dir2: 'right', // Position from the top left corner.
-      firstpos1: 10,
-      firstpos2: 10, // 10px from the top, 10px from the left.
-    }),
+    ...options,
   });
 }
 
 function searchingError() {
   return error({
     text: 'Error! Something went wrong.',
-    mode: 'light',
-    styling: 'brighttheme',
-    delay: 2000,
-    maxTextHeight: null,
-    stack: new Stack({
-      dir1: 'down',
-      dir2: 'right', // Position from the top left corner.
-      firstpos1: 10,
-      firstpos2: 10, // 10px from the top, 10px from the left.
-    }),
+    ...options,
   });
 }
 
